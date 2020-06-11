0b079bad7aed5a8a7a8441966f6a86b39e848288
FirePlank <sissala11@gmail.com>
2020-06-11 21:51:06 +0300

    Made more commands and created a custom help command

--- cogs/loadandunload.py
+++ cogs/loadandunload.py
@@ -9,12 +9,12 @@ class AdminCommands(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def load(self, extension):
+    async def load(self, ctx, extension):
         self.client.load_extension(f"cogs.{extension}")
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def unload(self, extension):
+    async def unload(self, ctx, extension):
         self.client.unload_extension(f"cogs.{extension}")
 
 
