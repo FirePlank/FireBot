958ac3d04217f83296a6f68404afba116789770a
FirePlank <sissala11@gmail.com>
2020-08-30 18:19:49 +0300

    fixed channe_health, imporved 8 ball

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = 'f.')
+client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game("f.help"))
+    await client.change_presence(activity=discord.Game(".help"))
 
 @client.event
 async def on_command_error(ctx, error):
@@ -27,5 +27,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-
-client.run("NzEzNzg5NTI2MDc3NjAzODUw.XslOTA.aXb-RpOHp-5Q91XosPKFN1SsG84")
+client.run(open("key.txt", "r").read())
