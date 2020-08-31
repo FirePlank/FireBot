620b3324fdea9a017220de385af23b22101a038e
Al Muhimen <almuhimen4@gmail.com>
2020-08-31 21:02:19 +0600

    Fixed Command Prefix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = 'f.')
 client.remove_command('help')
 
 @client.event
