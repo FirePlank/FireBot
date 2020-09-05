a1015d2d98d88ca54fd5f629a08b8c1545c367b5
FirePlank <44502537+FirePlank@users.noreply.github.com>
2020-09-05 16:14:51 +0300

    LEVELLING HAS BEEN ADDED!!

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-class AvatarCommands(commands.Cog):
+class HelpfulCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
     
@@ -10,19 +10,19 @@ class AvatarCommands(commands.Cog):
     async def av(self, ctx, *, members=None):
         mentions = ctx.message.mentions
         if len(mentions) == 0:
-            image_url = (ctx.author.avatar_url)
+            image_url = ctx.author.avatar_url
             author_name = ctx.author.name
             author = ctx.author
         else:
-            image_url = (mentions[0].avatar_url)
+            image_url = mentions[0].avatar_url
             author_name = mentions[0].name
             author = mentions[0]
 
-        message = discord.Embed(title=author_name, color=int('f59e42', 16))
+        message = discord.Embed(title=author_name, color=discord.Colour.orange())
         message.set_author(name=author, icon_url=image_url)
         message.set_image(url=image_url)
 
         await ctx.send(embed=message)
 
 def setup(client):
-    client.add_cog(AvatarCommands(client))
+    client.add_cog(HelpfulCommands(client))
