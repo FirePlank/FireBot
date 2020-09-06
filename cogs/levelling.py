b8ecd314b21b4b126020a65a7ff7585949a74c63
FirePlank <44502537+FirePlank@users.noreply.github.com>
2020-09-06 18:19:01 +0300

    Added set_lvl and set_exp commands and fixed levelling bug

--- cogs/levelling.py
+++ cogs/levelling.py
@@ -33,8 +33,7 @@ class AdminCommands(commands.Cog):
             if len(message.content) > 100: message_addon = 100/2.5
             else: message_addon = round(len(message.content)/2.5)
             new_exp = (exp+random.randint(10,20)*multiplier)+message_addon
-            await self.client.pg_con.execute("UPDATE levels SET exp = $1 WHERE guild_id = $2 and user_id = $3", new_exp, guild_id, author_id)
-            await self.client.pg_con.execute("UPDATE levels SET last_msg = $1 WHERE guild_id = $2 and user_id = $3", time.time(), guild_id, author_id)
+            await self.client.pg_con.execute("UPDATE levels SET exp = $1, last_msg = $2 WHERE guild_id = $3 and user_id = $4", new_exp, time.time(), guild_id, author_id)
 
             result2 = await self.client.pg_con.fetchrow("SELECT * FROM levels WHERE guild_id = $1 AND user_id = $2", guild_id, author_id)
             exp_start = int(result2["exp"])
@@ -46,12 +45,12 @@ class AdminCommands(commands.Cog):
                 elif lvl_start+1 == 10: await self.client.add_roles(message.author, 741008881563467989) ## TODO change these hardcoded roles to server specific
                 elif lvl_start+1 == 20: await self.client.add_roles(message.author, 741008953911017553) ## TODO change these hardcoded roles to server specific
 
-                await self.client.db.execute("UPDATE levels SET lvl = $1 and exp = $2 WHERE guild_id = $3 and user_id = $4", lvl_start+1, exp_start-exp_end, guild_id, author_id)
+                await self.client.pg_con.execute("UPDATE levels SET lvl = $1, exp = $2 WHERE guild_id = $3 and user_id = $4", lvl_start+1, exp_start-exp_end, guild_id, author_id)
 
     @commands.command()
     async def rank(self, ctx, user:discord.User=None):
-        result = await self.client.pg_con.fetchrow("SELECT * FROM levels WHERE guild_id = $1 AND user_id = $2", ctx.guild.id, ctx.author.id)
         if user is None:
+            result = await self.client.pg_con.fetchrow("SELECT * FROM levels WHERE guild_id = $1 AND user_id = $2", ctx.guild.id, ctx.author.id)
             if not result:
                 await ctx.send("You aren't ranked yet! Send messages to get exp.")
             else:
@@ -63,6 +62,7 @@ class AdminCommands(commands.Cog):
                 await ctx.send(embed=embed)
 
         else:
+            result = await self.client.pg_con.fetchrow("SELECT * FROM levels WHERE guild_id = $1 AND user_id = $2", ctx.guild.id, user.id)
             if not result:
                 await ctx.send("They aren't ranked yet! They have to send messages to get exp.")
             else:
@@ -85,6 +85,41 @@ class AdminCommands(commands.Cog):
         else:
             await self.client.pg_con.execute("UPDATE level_settings SET multiplier = $1 WHERE guild_id = $2", number, ctx.guild.id)
             await ctx.send(f"The exp multiplier has been updated to {number}")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def set_lvl(self, ctx, amount:int, user:discord.User=None):
+        if user is None: user = ctx.author
+        guild_id = ctx.guild.id
+
+        result = await self.client.pg_con.fetchval("SELECT lvl FROM levels WHERE guild_id = $1 and user_id = $2", guild_id, user.id)
+        if result is None:
+            await self.client.pg_con.execute("INSERT INTO levels(guild_id, user_id, exp, lvl, last_msg) VALUES($1,$2,$3,$4,$5)",  guild_id, user.id, 0, amount, time.time()-60)
+        else:
+            await self.client.pg_con.execute("UPDATE levels SET lvl = $1 WHERE guild_id = $2 and user_id = $3", amount, guild_id, user.id)
+        await ctx.send(f"{user.name}'s level has been set to {amount}")
+
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def set_exp(self, ctx, amount: int, user: discord.User = None):
+        if user is None: user = ctx.author
+        guild_id = ctx.guild.id
+
+        result = await self.client.pg_con.fetchval("SELECT exp FROM levels WHERE guild_id = $1 and user_id = $2", guild_id, user.id)
+        if result is None:
+            if amount>=100:
+                await ctx.send("You cannot give more exp than their current level exp requirement is")
+                return
+            await self.client.pg_con.execute("INSERT INTO levels(guild_id, user_id, exp, lvl, last_msg) VALUES($1,$2,$3,$4,$5)", guild_id, user.id, amount, 0, time.time() - 60)
+        else:
+            result = await self.client.pg_con.fetchval("SELECT lvl FROM levels WHERE guild_id = $1 and user_id = $2", guild_id, user.id)
+            if amount>=math.floor(5*((int(result)+1)**2)+50*(int(result)+1)+100):
+                await ctx.send("You cannot give more exp than their current level exp requirement is")
+                return
+            await self.client.pg_con.execute("UPDATE levels SET exp = $1 WHERE guild_id = $2 and user_id = $3", amount, guild_id, user.id)
+
+        await ctx.send(f"{user.name}'s exp has been set to {amount}")
 
 
 def setup(client):
