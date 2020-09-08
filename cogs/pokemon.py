76bf1aaee1995b292a624eedd0714d045c5691e0
FirePlank <sissala11@gmail.com>
2020-09-08 20:55:54 +0300

    fixed Saykat's sloppy code lol

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -9,23 +9,19 @@ class FunCommands(commands.Cog):
         self.client = client
 
     @commands.command(aliases=['pokedex'])
-    async def poke_info(sefl, ctx, *, name=None):
-        if name == None:
-            message = discord.Embed(title="Please Provide a Name", color=discord.Colour.orange())
-            await ctx.send(embed=message)
-
-        elif check_name(name):
+    async def poke_info(self, ctx, *, name):
+        if len(name.split(" "))==1:
             try:
-                response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+                response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name.lower()}")
                 data = response.json()
                 
                 poke_image = get_image(data)
                 hp = data['base_experience']
                 name = data['name'].title()
-                height = str(data['height'] / 10) + "meters"
+                height = str(data['height'] / 10) + "m"
                 weight = str(data['weight'] / 10) + "kg"
                 catagory = data['types'][0]['type']['name'].title()
-                ability = [d['ability']['name'] for d in data['abilities']]
+                ability = [d['ability']['name'].capitalize() for d in data['abilities']]
 
                 message = discord.Embed(title=name, color=discord.Colour.orange())
 
@@ -38,30 +34,20 @@ class FunCommands(commands.Cog):
 
                 await ctx.send(embed=message)
 
-            except Exception as e:
-                print(e)
+            except:
                 message = discord.Embed(title="Sorry, no Pokemon found", color=discord.Colour.orange())
                 await ctx.send(embed=message)
 
         else:
             message = discord.Embed(title="The name cannot contain two or more words", color=discord.Colour.orange())
+            await ctx.send(embed=message)
 
-
-
-
-def check_name(name):
-    name = name.split()
-    if len(name) != 1:
-        return False
-    else:
-        return True
 
 def get_image(data):
     try:
         image = data['sprites']['other']['official-artwork']['front_default']
     except:
         image = data['sprites']['front_shiny']
-    print(image)
     return image
 
 def setup(client):
