eba4903f80b14499bf927e002acc1e205402fd49
FirePlank <44502537+FirePlank@users.noreply.github.com>
2020-10-26 20:27:34 +0200

    Fixed av command

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -5,13 +5,13 @@ from discord.ext import commands
 class HelpfulCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
-    @commands.command()
-    async def av(self, ctx, *, user: discord.Member = None):
-        if not user:user=ctx.author
 
-        message = discord.Embed(title=user, color=discord.Colour.orange())
-        message.set_image(url=user.avatar_url)
+    @commands.command(aliases=["av", "pfp"])
+    async def avatar(self, ctx, *, member: discord.Member = None):
+        if not member:member=ctx.message.author
+
+        message = discord.Embed(title=str(member), color=discord.Colour.orange())
+        message.set_image(url=member.avatar_url)
 
         await ctx.send(embed=message)
 
