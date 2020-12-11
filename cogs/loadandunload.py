df29b8ef113d9f3bb78b2ca5236a68e6888563da
arnav03mehta <arnav2003mehta@gmail.com>
2020-12-11 11:42:05 +0530

    Added error protection and a reload comand

--- cogs/loadandunload.py
+++ cogs/loadandunload.py
@@ -10,15 +10,30 @@ class AdminCommands(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def load(self, ctx, extension):
-        self.client.load_extension(f"cogs.{extension}")
-        await ctx.send(f"The module {extension} has been loaded successfully!")
+        try:
+            self.client.load_extension(f"cogs.{extension}")
+            await ctx.send(f"The module {extension} has been loaded successfully!")
+        except discord.ext.commands.errors.ExtensionNotFound:
+            await ctx.send(f"The module {extension} does not exist")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def reload(self, ctx, extension):
+        try:
+            self.client.unload_extension(f"cogs.{extension}")
+            self.client.load_extension(f"cogs.{extension}")
+            await ctx.send(f"The module {extension} has been reloaded successfully!")
+        except discord.ext.commands.errors.ExtensionNotLoaded:
+            await ctx.send(f"The module {extension} either is not loaded or doesn't exist")
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def unload(self, ctx, extension):
-        self.client.unload_extension(f"cogs.{extension}")
-        await ctx.send(f"The module '{extension}' has been unloaded successfully!")
-
+        try:
+            self.client.unload_extension(f"cogs.{extension}")
+            await ctx.send(f"The module '{extension}' has been unloaded successfully!")
+        except discord.ext.commands.errors.ExtensionNotLoaded:
+            await ctx.send(f"The module {extension} either isn't loaded or doesn't exist")
 
 def setup(client):
     client.add_cog(AdminCommands(client))
