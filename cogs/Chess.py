88b4c6c812bdd2d9d0e620b19d8414ef7d5ec596
PC\FirePlank <sissala11@gmail.com>
2020-12-11 23:25:17 +0200

    Added automod for mass pinging and other small addons.

---
+++ cogs/Chess.py
@@ -0,0 +1,17 @@
+import discord
+import chess
+from discord.ext import commands
+
+class Fun(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['chess_play'])
+    async def chess_invite(self, ctx, user:discord.Member):
+        if user.bot:return ctx.send("You cannot invite a bot.")
+
+
+
+def setup(client):
+    client.add_cog(Fun(client))
