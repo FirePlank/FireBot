873f0424020ad0c62c3a213577dd6cc32afb7478
PC\FirePlank <sissala11@gmail.com>
2020-12-13 19:22:50 +0200

    Added tags and did improvements

--- cogs/unmute.py
+++ cogs/unmute.py
@@ -8,17 +8,17 @@ class AdminCommands(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def mute(self, ctx, user:discord.Member):
+    async def unmute(self, ctx, user:discord.Member):
         muted_role = discord.utils.find(lambda r: r.name.upper() == 'MUTED', ctx.guild.roles)
         staff_role = discord.utils.find(lambda r: r.name.upper() == 'STAFF', ctx.guild.roles)
         if staff_role not in ctx.author.roles:
             return await ctx.send("Sorry. You don't have the permission for that command.")
         if muted_role:
-            if muted_role in user:
-                return await ctx.send("That person is already muted!")
+            if muted_role not in user:
+                return await ctx.send("That person is not currently muted!")
 
-            await user.add_roles(muted_role)
-            await ctx.send(f"{user} has been muted.")
+            await user.remove_roles(muted_role)
+            await ctx.send(f"{user} has been unmuted.")
         else:
             await ctx.send("Couldn't find a role with 'muted' in the name.")
 
