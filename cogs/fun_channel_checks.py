1bc2bbfd9847e66524da7e22b65111cf943a0d07
class PythonAddict <72556571+classPythonAddike@users.noreply.github.com>
2021-04-22 20:16:52 +0530

    Added code execution

--- cogs/fun_channel_checks.py
+++ cogs/fun_channel_checks.py
@@ -2,6 +2,8 @@ import discord
 import random
 from discord.ext import commands
 import asyncpg
+import requests
+import os
 
 class AdminCommands(commands.Cog):
 
@@ -57,13 +59,42 @@ class AdminCommands(commands.Cog):
                 with open("cogs/code.txt", 'a') as f:
                     f.write(code + "\n")
 
-                await channel.send(f"Code added! Code so far:\n```{open('cogs/code.txt', 'r').read()}```")
+                await channel.send(f"Code added! Code so far:\n```py\n{open('cogs/code.txt', 'r').read()}```")
 
             elif message.content == "run":
                 with open("cogs/code.txt", 'r+') as f:
                     read_file = f.read()
                     f.truncate(0)
-                await channel.send(f"Code completed! The code is:\n```{read_file}```")
+
+                data = {
+                    "script": str(read_file),
+                    "language": "python3",
+                    "versionIndex": "3",
+                    "clientId": os.environ["clientId"],
+                    "clientSecret": os.environ["clientSecret"],
+                    "stdin": ""
+                }
+
+                result = requests.post("https://api.jdoodle.com/v1/execute", json=data).json()
+
+                if result["statusCode"] == 200:
+                    message = discord.Embed(title="Compilation Results", colour=discord.Colour.orange())
+                    message.add_field(name="Program Output", value=f'```{result["output"]}```', inline=False)
+                    message.add_field(name="Execution Time", value=result["cpuTime"], inline=False)
+                    message.set_footer(text=f"Requested by: {message.author} | Powered by Jdoodle")
+                else:
+                    message = discord.Embed(title="Compilation Results", colour=discord.Colour.blue())
+                    message.add_field(name="Error", value=result['error'], inline=False)
+                    message.set_footer(text=f"Requested by: {str(message.author)} | Powered by Jdoodle")
+
+                await channel.send(embed=message)
+
+            elif message.content == "code":
+                with open("cogs/code.txt", 'r+') as f:
+                    read_file = f.read()
+                    f.truncate(0)
+
+                await channel.send(f"Code so far:\n```py\n{read_file}```")
 
 def setup(client):
     client.add_cog(AdminCommands(client))
