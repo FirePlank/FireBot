4117f19978246a624828f0a7f8888c5e05db4a15
FirePlank <44502537+FirePlank@users.noreply.github.com>
2021-04-25 15:42:16 +0300

    updated lockdown command to work with default role

--- cogs/channel_lock.py
+++ cogs/channel_lock.py
@@ -11,11 +11,11 @@ class AdminCommands(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def lockdown(self, ctx, channel: discord.TextChannel = None):
         channel = channel or ctx.channel
-        verified_role = ctx.guild.get_role(741012501445214258)
+        role = ctx.guild.default_role
 
-        if verified_role not in channel.overwrites:
+        if role not in channel.overwrites:
             overwrites = {
-                verified_role: discord.PermissionOverwrite(send_messages=False)
+                role: discord.PermissionOverwrite(send_messages=False)
             }
             await channel.edit(overwrites=overwrites)
             if ctx.channel != channel:
@@ -23,20 +23,20 @@ class AdminCommands(commands.Cog):
             else:
                 await ctx.message.delete()
             await channel.send(embed=discord.Embed(title="This channel is now under lockdown", color=discord.Colour.orange()))
-        elif channel.overwrites[verified_role].send_messages is True or \
-                channel.overwrites[verified_role].send_messages is None:
-            overwrites = channel.overwrites[verified_role]
+        elif channel.overwrites[role].send_messages is True or \
+                channel.overwrites[role].send_messages is None:
+            overwrites = channel.overwrites[role]
             overwrites.send_messages = False
-            await channel.set_permissions(verified_role, overwrite=overwrites)
+            await channel.set_permissions(role, overwrite=overwrites)
             if ctx.channel != channel:
                 await ctx.send(f"I have put {channel.mention} on lockdown.")
             else:
                 await ctx.message.delete()
             await channel.send(embed=discord.Embed(title="This channel is now under lockdown.", color=discord.Colour.orange()))
         else:
-            overwrites = channel.overwrites[verified_role]
+            overwrites = channel.overwrites[role]
             overwrites.send_messages = True
-            await channel.set_permissions(verified_role, overwrite=overwrites)
+            await channel.set_permissions(role, overwrite=overwrites)
             if ctx.channel != channel:
                 await ctx.send(f"I have removed {channel.mention} from lockdown.")
             else:
